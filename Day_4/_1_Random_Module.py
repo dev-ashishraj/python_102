10fc739f100f89196c78e78c2217df0be05a9351
Ashish Raj <ashishraj.dev@proton.me>
2026-03-16 19:32:01 +0530

    Random Module

--- Day_4/_1_Random_Module.py
+++ Day_4/_1_Random_Module.py
@@ -2,7 +2,17 @@ import random
 import _2_my_module
 
 
-random_imteger = random.randint(1,10)
-print(random_imteger)
+# random_imteger = random.randint(1,10)
+# print(random_imteger)
 
-print(_2_my_module.my_favourite_number)
+# print(_2_my_module.my_favourite_number)
+
+# randome_numbers = random.random()
+# print(randome_numbers) #("In this line if code we are seening to random, Here one is called as 'module' and anothere is called as 'function'. How can we figer out that which one function and which one is module") ("In this line code the first randome is concidered as 'Module' and the second one is 'function'.")
+# And this code "random.random" only contant the value btw 0_to_1. If we want to incress the value we have to multile this code by the number we want like:-
+randome_numbers_= random.random() * 8 
+print(randome_numbers_)
+
+
+# randome_floating = random.uniform(1,10)
+# print(randome_floating)
